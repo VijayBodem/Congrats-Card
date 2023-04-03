c926dbe504b90e44fbd62fe6200f77bab45c206a
vijayBodem <vijaybodem17@gmail.com>
2023-04-03 08:11:58 +0000

    Congrats Card

---
+++ index.js
@@ -0,0 +1,14 @@
+const element = (
+  // Write your code here.
+  <div className="bg-container">
+    <h1 className="heading">Congratulations</h1>
+    <div className="congrats-card">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1>Vijay B</h1>
+      <p>Vijay institue of computer education and technology</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
